822add466624ef775df82f08891e697dfd31fd45
Chakyl <chakyl1127@gmail.com>
2025-03-16 17:36:03 -0400

    stardew fishing 2!!!

--- kubejs/server_scripts/loot/fishingLoot.js
+++ kubejs/server_scripts/loot/fishingLoot.js
@@ -19,7 +19,29 @@ LootJS.modifiers((e) => {
     .pool((p) => {
       p.randomChance(0.2).addLoot("aquaculture:treasure_chest");
     });
-
+  e.addLootTypeModifier(LootType.FISHING)
+    .playerPredicate((p) =>
+      p
+        .getHeldItem("main_hand")
+        .nbt.Inventory.Items.toString()
+        .contains("aquaculture:iron_hook")
+    )
+    .pool((p) => {
+      p.randomChance(0.5).addLoot("crabbersdelight:crab_trap_bait");
+      p.limitCount([4, 16]);
+    });
+  e.addLootTypeModifier(LootType.FISHING)
+    .playerPredicate((p) =>
+      p
+        .getHeldItem("main_hand")
+        .nbt.Inventory.Items.toString()
+        .contains("aquaculture:diamond_hook")
+    )
+    .pool((p) => {
+      console.log(p);
+      p.randomChance(0.25).addLoot("crabbersdelight:deluxe_crab_trap_bait");
+      p.limitCount([1, 4]);
+    });
   e.addLootTypeModifier(LootType.CHEST)
     .hasAnyStage("prismatic_bounty")
     .replaceLoot(
